1c7165ce2c03e0a894c36e3e7f0653ecf48a35d4
Zephyr <frastxini@gmail.com>
2025-07-23 00:58:31 +0300

    Minor changes

---
+++ bin/src/MinMoves/MinMovesService.java
@@ -0,0 +1,10 @@
+package MinMoves;
+import java.util.Arrays;
+import java.util.OptionalDouble;
+
+public class MinMovesService {
+    public static int minMoves2(int[] nums) {
+        OptionalDouble nums_avg = Arrays.stream(nums).average();
+        return 1;
+    }
+}
